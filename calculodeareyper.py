7b42d2a17292b5e65c4a31d896b0efb7886d5255
Adan909 <kalacuna@uamv.edu.ni>
2025-04-02 13:56:15 -0600

    Añadi python

---
+++ calculodeareyper.py
@@ -0,0 +1,5 @@
+base= float(input("Ingresa el area del rectangulo: "))
+altura= float(input("Ingresa la altura del rectangulo: "))
+area= base*altura
+perimetro= (base*2)+(altura*2)
+print("El area de tu rectangulo es: ", area, " y su perimetro es de: ", perimetro)
---
+++ ejemplo1.py
@@ -0,0 +1,7 @@
+#Calcular la nota de un estudiante, que tiene 2 sistematicos de 30
+# y uno de 40
+sistematico1= int(input("Ingrese primer sistematico(Max30): "))
+sistematico2= int(input("Ingrese segundo sistematico(Max30): "))
+sistematico3= int(input("Ingrese tercer sistematico(Max40): "))
+nota = (sistematico1+sistematico2+sistematico3)
+print("La nota del estudiante es: ", nota)
